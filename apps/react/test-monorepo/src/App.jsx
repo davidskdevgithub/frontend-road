18f406198b1ab31548510a70742764f3440defed
davidskdevgithub <davidsk.dev@gmail.com>
2023-04-06 21:33:39 -0300

    feat: get package from app react

--- apps/react/test-monorepo/src/App.jsx
+++ apps/react/test-monorepo/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { Title } from 'ui-react'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -16,7 +18,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <Title text="New title" />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
